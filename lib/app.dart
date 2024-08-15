ea3871878c379c9dbe2271685da2f3b6fb2f2202
anhquantr123 <anhquan.lien123@gmail.com>
2024-08-15 07:04:53 +0700

    config main

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:user_repository/user_repopsitory.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+
+import 'blocs/authentication_bloc/authentication_bloc.dart';
 
 class MyApp extends StatelessWidget {
   final UserRepository userRepository;
@@ -7,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RepositoryProvider<AuthenticationBloc>(
+      create: (context) => AuthenticationBloc(userRepository: UserRepository),
+      child: MyAppView(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/simple_bloc_observer.dart';
-import 'package:user_repository/user_repopsitory.dart';
+import 'package:user_repository/user_repository.dart';
 
 import 'app.dart';
 
